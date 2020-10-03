2a1d1bbf7235f2dbfb61b3674b513c9451f719b9
Kiss Márk Bence <kiss.bence@exinko.net>
2020-10-03 14:53:34 +0200

    feat(copyright) create a copyright component

---
+++ src/app/shared/copyright/copyright.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { CopyService } from '../copy.service';
+
+@Component({
+  selector: 'app-copyright',
+  templateUrl: './copyright.component.html',
+  styleUrls: ['./copyright.component.scss']
+})
+export class CopyrightComponent implements OnInit, OnDestroy {
+
+	public isCopiedSwitch = false;
+	public copySubscription: Subscription;
+
+	constructor(private copyService: CopyService) { }
+
+
+	public ngOnInit(): void {
+		this.copySubscription = this.copyService.copiedSubject.subscribe((status) => {
+			this.isCopiedSwitch = status;
+		});
+	}
+
+	public copyEmail(event: MouseEvent) {
+		const textContent = (event.target as HTMLElement).textContent;
+		this.copyService.copyEmail(textContent);
+	}
+
+	public ngOnDestroy(): void {
+		this.copySubscription.unsubscribe();
+	}
+}
