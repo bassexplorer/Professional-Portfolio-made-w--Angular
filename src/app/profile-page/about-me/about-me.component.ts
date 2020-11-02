c54ad23df1f6d38bdec27ee5e570e19bc610d3c1
Bence <bassexplorer.beni@gmail.com>
2020-11-02 11:39:34 +0100

    fix about responsiveness

--- src/app/profile-page/about-me/about-me.component.ts
+++ src/app/profile-page/about-me/about-me.component.ts
@@ -1,5 +1,5 @@
 import { DragRef } from '@angular/cdk/drag-drop';
-import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Subscription } from 'rxjs';
 
 import { BreakpointService } from '../../shared/breakpoint.service';
@@ -12,21 +12,11 @@ import { BreakpointService } from '../../shared/breakpoint.service';
 export class AboutMeComponent implements OnInit {
 
 	public onIpadPro: boolean;
-	public visibleCopyright = true;
 	public breakpointObserverSubscription: Subscription;
 	public breakpointObs$;
 	@ViewChild('dragRef', { static: true }) public dragRef: DragRef;
 	constructor(private breakpointService: BreakpointService) {
 		this.breakpointObs$ = this.breakpointService.breakpoints;
-	}
-
-	@HostListener('window:scroll', ['$event']) public onScroll(event) {
-		const pageYOffset = event.path[1].pageYOffset;
-		if (pageYOffset > 100) {
-			this.visibleCopyright = false;
-		} else {
-			this.visibleCopyright = true;
-		}
 	}
 
 	public ngOnInit(): void { }
