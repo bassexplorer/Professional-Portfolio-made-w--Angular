1884e8830604022ed03cfd94ae6fc850ead99e6d
Bence <bassexplorer.beni@gmail.com>
2020-11-02 11:36:49 +0100

    Make contact form work

--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { ContactService } from './contact.service';
 
@@ -16,27 +16,32 @@ import { ContactService } from './contact.service';
 export class ContactPageComponent implements OnInit {
 
 	public contactForm: FormGroup;
+	public onConfirmation: boolean;
 
-	constructor(private contact: ContactService) { }
+	constructor(private contact: ContactService, private builder: FormBuilder) { }
 
 
 
 	public ngOnInit(): void {
-		this.contactForm = new FormGroup({
+		this.contactForm = this.builder.group({
 			fullName: new FormControl('', [Validators.required]),
 			email: new FormControl('', [Validators.compose([Validators.required, Validators.email])]),
 			subject: new FormControl('', [Validators.required]),
 			message: new FormControl('', [Validators.required])
 		});
+
+		this.onConfirmation = false;
 	}
 
 
 
-	public onSubmit() {
-		this.contact.PostMessage(FormData)
+	public onSubmit(formData) {
+		// this.contact.PostMessage(JSON.stringify(this.contactForm.value))
+		this.contact.PostMessage(formData)
 			.subscribe((response) => {
-				location.href = 'https://mailthis.to/confirm'
-			}, error => {
+				this.onConfirmation = true;
+				location.href = 'https://mailthis.to/confirm';
+			}, (error) => {
 				console.warn(error.responseText);
 			});
 	}
